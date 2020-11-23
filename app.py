a97b5b3a5d988d2f2df823fa0b497e02247a4425
anurag sharma <anu091716@gmail.com>
2020-11-23 17:55:15 +0530

    config.ini

---
+++ app.py
@@ -0,0 +1,125 @@
+# Classic libraries
+import os
+import numpy as np
+import pandas as pd
+
+# Logging information
+import logging
+import logzero
+from logzero import logger
+
+# Dash imports
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Custom function
+import scripts.utils_covid as f
+
+# Load pre computed data
+world = f.load_pickle('world_info.p')
+
+# Deployment inforamtion
+PORT = 8050
+
+############################################################################################
+########################################## APP #############################################
+############################################################################################
+
+# Creating app
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+
+# Associating server
+server = app.server
+app.title = 'COVID 19 - World cases'
+app.config.suppress_callback_exceptions = True
+
+############################################################################################
+######################################### LAYOUT ###########################################
+############################################################################################
+
+
+links = html.Div(
+    id='platforms_links',
+    children=[                   
+        html.A(
+            href='https://www.linkedin.com/in/anurag-sharma-0308/',
+            children=[
+                html.Img(src=app.get_asset_url('linkedin.png'), width=20, height=20),
+                html.Span("Map")
+            ]
+        ),
+        html.A(
+            href='https://github.com/anurag0308',
+            children=[
+                html.Img(src=app.get_asset_url('github.png'), width=20, height=20),
+                # "Application code"
+                html.Span("Code")
+            ]
+        ),
+        html.A(
+            href='https://public.opendatasoft.com/explore/dataset/covid-19-pandemic-worldwide-data/information/?disjunctive.zone&disjunctive.category&sort=date',
+            children=[
+                html.Img(src=app.get_asset_url('database.png'), width=20, height=20),
+                # "Original COVID dataset"
+                html.Span("Data")
+            ],
+        ),
+    ],
+)
+
+app.layout = html.Div(
+    children=[
+
+        # HEADER
+        html.Div(
+            className="header",
+            children=[
+                html.H1("COVID 19 🦠 - Day to day evolution all over the world", className="header__text"),
+                html.Span('(Last update: {})'.format(world['last_date'])),
+                # html.Hr(),
+            ],
+        ),
+
+        # CONTENT
+        html.Section([
+            
+            # Line 1 : KPIS - World
+            html.Div(
+                id='world_line_1',
+                children = [ 
+                    html.Div(children = ['🚨 Confirmed', html.Br(), world['total_confirmed']], id='confirmed_world_total', className='mini_container'),
+                    html.Div(children = ['🏡 Recovered', html.Br(), world['total_recovered']], id='recovered_world_total', className='mini_container'),
+                    html.Div(children = [' ⚰️ Victims',   html.Br(), world['total_deaths']],    id='deaths_world_total',    className='mini_container'),            
+                ],
+            ),
+            # html.Br(),
+            links,
+
+            # Line 2 : MAP - WORLD
+            html.Div(
+                id='world_line_2',
+                children = [
+                    dcc.Graph(id='world_map', figure=world['figure'], config={'scrollZoom': False}),         
+                ],
+            ),
+            # html.Br(),
+        ]),
+    ],
+)
+
+############################################################################################
+######################################### RUNNING ##########################################
+############################################################################################
+
+if __name__ == "__main__":
+    
+    # Display app start
+    logger.error('*' * 80)
+    logger.error('App initialisation')
+    logger.error('*' * 80)
+
+    # Starting flask server
+    app.run_server(debug=True, port=PORT)
---
+++ utils_covid.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+from configparser import ConfigParser 
+
+config = ConfigParser()
+
+if 'config.ini' in os.listdir():
+    config.read('config.ini')
+else:
+    config.read('../config.ini')
+
+# Create path for project use
+DATA_PATH = config['path']['data']
+RAW_PATH = DATA_PATH + 'raw/'
+PICKLE_PATH = DATA_PATH + 'pickle/'
+
+def load_pickle(file_name):
+    file_path = PICKLE_PATH + file_name
+    with open(file_path, 'rb') as pfile:
+        my_pickle = pickle.load(pfile)
+    return my_pickle
+
+def save_pickle(object_, file_name):
+    file_path = PICKLE_PATH + file_name
+    with open(file_path, 'wb') as pfile:
+        # pickle.dump(object_, pfile, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(object_, pfile, protocol=2)
+
+def list_pickle():
+    file_list = os.listdir(PICKLE_PATH)
+    pickle_list = [i for i in file_list if '.p' in i]
+    print(pickle_list)
+    
+def spacify_number(number):
+    nb_rev = str(number)[::-1]
+    new_chain = ''
+    for val, letter in enumerate(nb_rev):
+        if val%3==0:
+            new_chain += ' '
+        new_chain += letter
+    final_chain = new_chain[::-1]
+    return final_chain
+    
